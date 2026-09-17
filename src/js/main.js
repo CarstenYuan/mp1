e0b076dd56cb72c6a759e1638e9e256560df68ff
hawyuan2 <hawyuan2@illinois.edu>
2026-09-16 22:32:09 -0500

    Add navbar scrolling animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+// Navbar
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function () {
+    console.log(window.scrollY);
+
+    if (window.scrollY > 60) {
+        navbar.classList.add('navbar--scrolled');
+    } else {
+        navbar.classList.remove('navbar--scrolled');
+    }
+});
+
+window.addEventListener('load', function () {
+    navbar.classList.add('navbar--ready');
+});
